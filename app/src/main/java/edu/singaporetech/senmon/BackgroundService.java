a766f85271a1c37ba64ecfd91d243cb0866246ae
jianhong92 <zenboyz@hotmail.com>
2016-06-30 14:56:39 +0800

    fix warning notifications to values from settingsfragment

--- app/src/main/java/edu/singaporetech/senmon/BackgroundService.java
+++ app/src/main/java/edu/singaporetech/senmon/BackgroundService.java
@@ -6,11 +6,13 @@ import android.app.Service;
 import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.IBinder;
 import android.os.Vibrator;
+import android.preference.PreferenceManager;
 import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 
@@ -22,6 +24,14 @@ public class BackgroundService extends Service{
     private boolean isRunning;
     private Context context;
     private Thread backgroundThread;
+
+    public static final String MyPREFERENCES = "MyPrefs" ;
+    public static final String NotificationsEnabled = "ntfnEnabledKey";
+    public static final String WarningEnabled = "warningEnabledKey";
+    public static final String CriticalEnabled = "criticalEnabledKey";
+    public static final String FavNtfnOnly = "favNtfnOnlyKey";
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
 
     NotificationManager notificationManager;
     int WarnNotificID = 111;
@@ -37,6 +47,7 @@ public class BackgroundService extends Service{
         this.context = this;
         this.isRunning = false;
         this.backgroundThread = new Thread(mTask);
+
     }
 
     private Runnable mTask = new Runnable() {
@@ -44,12 +55,52 @@ public class BackgroundService extends Service{
         public void run() {
             //TODO do something here
             Log.d("LOG", "BACKGROUND IS RUNNING");
+            sharedPreferences = context.getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+//            sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
 
-//            NotificationCompat.Builder WarnNotificBuilder = (NotificationCompat.Builder) new
+            if(sharedPreferences.getBoolean(NotificationsEnabled,true)) {
+                Log.d("LOL", "SERVICE RECEIVED THE VARIABLE");
+                if (sharedPreferences.getBoolean(WarningEnabled, true)) {
+                    Log.d("LOL", "SERVICE RECEIVED THE WARNING ENABLED VARIABLE");
+                    NotificationCompat.Builder WarnNotificBuilder = (NotificationCompat.Builder) new
+                            NotificationCompat.Builder(context).setContentTitle("Attention!")
+                            .setContentText("Machine is in Warning State!")
+                            .setTicker("Machine needs attention!")
+                            .setSmallIcon(R.drawable.ic_warning_white_48dp)
+                            .setAutoCancel(true);
+                    Intent openIntent = new Intent(context, MainActivity.class);
+
+                    TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(context);
+                    taskStackBuilder.addParentStack(MainActivity.class);
+                    taskStackBuilder.addNextIntent(openIntent);
+
+                    PendingIntent pendingIntent = taskStackBuilder.getPendingIntent(0,
+                            PendingIntent.FLAG_UPDATE_CURRENT);
+
+                    //TODO fire off the notification for warning machines *temp holding place
+//                    WarnNotificBuilder.setContentIntent(pendingIntent);
+//
+//                    notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+//                    notificationManager.notify(WarnNotificID, WarnNotificBuilder.build());
+//                    Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+//                    v.vibrate(1000);
+//
+//                    try {
+//                        Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+//                        Ringtone r = RingtoneManager.getRingtone(context.getApplicationContext(), notification);
+//                        r.play();
+//                    } catch (Exception e) {
+//                        e.printStackTrace();
+//                    }
+//                    isWarnNotificActive = true;
+                }
+            }
+////            //TODO build notifications for critical machines
+//            NotificationCompat.Builder CritNotificBuilder = (NotificationCompat.Builder) new
 //                    NotificationCompat.Builder(context).setContentTitle("Attention!")
-//                    .setContentText("Machine is in Warning State!")
+//                    .setContentText("Machine is in Critical State!")
 //                    .setTicker("Machine needs attention!")
-//                    .setSmallIcon(R.drawable.ic_warning_white_48dp)
+//                    .setSmallIcon(R.drawable.ic_cancel_white_48dp)
 //                    .setAutoCancel(true);
 //            Intent openIntent = new Intent(context, MainActivity.class);
 //
@@ -60,11 +111,11 @@ public class BackgroundService extends Service{
 //            PendingIntent pendingIntent = taskStackBuilder.getPendingIntent(0,
 //                    PendingIntent.FLAG_UPDATE_CURRENT);
 //
-//            //TODO fire off the notification for warning machines *temp holding place
-//            WarnNotificBuilder.setContentIntent(pendingIntent);
+//            //TODO fire off the critical notification
+//            CritNotificBuilder.setContentIntent(pendingIntent);
 //
 //            notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-//            notificationManager.notify(WarnNotificID, WarnNotificBuilder.build());
+//            notificationManager.notify(CritNotificID, CritNotificBuilder.build());
 //            Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
 //            v.vibrate(1000);
 //
@@ -75,7 +126,7 @@ public class BackgroundService extends Service{
 //            } catch (Exception e) {
 //                e.printStackTrace();
 //            }
-//            isWarnNotificActive = true;
+//            isCritNotificActive = true;
 
             //call the destroy method
             stopSelf();
--- app/src/main/java/edu/singaporetech/senmon/SettingsFragment.java
+++ app/src/main/java/edu/singaporetech/senmon/SettingsFragment.java
@@ -130,45 +130,6 @@ public class SettingsFragment extends Fragment implements android.widget.Compoun
 
                 if(buttonView.isChecked()) {        // warning notifications enabled
                     editor.putBoolean(WarningEnabled, true);
-                    //TODO build notifications for warning machines
-                    NotificationCompat.Builder WarnNotificBuilder = (NotificationCompat.Builder) new
-                            NotificationCompat.Builder(context).setContentTitle("Attention!")
-                            .setContentText("Machine is in Warning State!")
-                            .setTicker("Machine needs attention!")
-                            .setSmallIcon(R.drawable.ic_warning_white_48dp)
-                            .setAutoCancel(true);
-                    Intent openIntent = new Intent(context, MainActivity.class);
-
-                    TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(context);
-                    taskStackBuilder.addParentStack(MainActivity.class);
-                    taskStackBuilder.addNextIntent(openIntent);
-
-                    PendingIntent pendingIntent = taskStackBuilder.getPendingIntent(0,
-                            PendingIntent.FLAG_UPDATE_CURRENT);
-
-                    //TODO fire off the notification for warning machines *temp holding place
-                    WarnNotificBuilder.setContentIntent(pendingIntent);
-
-                    notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-                    notificationManager.notify(WarnNotificID, WarnNotificBuilder.build());
-                    Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
-                    v.vibrate(1000);
-
-                    try {
-                        Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-                        Ringtone r = RingtoneManager.getRingtone(context.getApplicationContext(), notification);
-                        r.play();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    isWarnNotificActive = true;
-                }
-                else {
-                    //TODO if the switch is off then cancel notifications
-                    editor.putBoolean(WarningEnabled, false);
-                    if(isWarnNotificActive){
-                        notificationManager.cancel(WarnNotificID);
-                    }
                 }
 
                 break;
@@ -176,38 +137,7 @@ public class SettingsFragment extends Fragment implements android.widget.Compoun
 
                 if(buttonView.isChecked()) {        // critical notifications enabled
                     editor.putBoolean(CriticalEnabled, true);
-                    //TODO build notifications for critical machines
-                    NotificationCompat.Builder CritNotificBuilder = (NotificationCompat.Builder) new
-                            NotificationCompat.Builder(context).setContentTitle("Attention!")
-                            .setContentText("Machine is in Critical State!")
-                            .setTicker("Machine needs attention!")
-                            .setSmallIcon(R.drawable.ic_cancel_white_48dp)
-                            .setAutoCancel(true);
-                    Intent openIntent = new Intent(context, MainActivity.class);
 
-                    TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(context);
-                    taskStackBuilder.addParentStack(MainActivity.class);
-                    taskStackBuilder.addNextIntent(openIntent);
-
-                    PendingIntent pendingIntent = taskStackBuilder.getPendingIntent(0,
-                            PendingIntent.FLAG_UPDATE_CURRENT);
-
-                    //TODO fire off the critical notification
-                    CritNotificBuilder.setContentIntent(pendingIntent);
-
-                    notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-                    notificationManager.notify(CritNotificID, CritNotificBuilder.build());
-                    Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
-                    v.vibrate(1000);
-
-                    try {
-                        Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-                        Ringtone r = RingtoneManager.getRingtone(context.getApplicationContext(), notification);
-                        r.play();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    isCritNotificActive = true;
                 }
                 else {
                     editor.putBoolean(CriticalEnabled, false);
