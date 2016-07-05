0bdd042bf0a5165ccee0b0d540ef93083475834c
meixi-l <lim.m3ixi@gmail.com>
2016-07-05 17:29:59 +0800

    change list tablayout to not use viewpager

--- app/src/main/java/edu/singaporetech/senmon/ListFragment.java
+++ app/src/main/java/edu/singaporetech/senmon/ListFragment.java
@@ -15,6 +15,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.util.Property;
 import android.view.LayoutInflater;
@@ -41,9 +42,11 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
@@ -57,16 +60,33 @@ import static java.lang.String.CASE_INSENSITIVE_ORDER;
  */
 public class ListFragment extends Fragment {
 
-    public ArrayList<Machine> myMachineList = new ArrayList<Machine>();
-    public ArrayList<String> machineArray = new ArrayList<String>();
-
     String TAG = "List Fragment";
     Context context;
 
+    private static final String TAG_RESULTS="result";
+
+    ListView listViewListing;
+    CustomAdapter adapter;
+
+    public ArrayList<Machine> myMachineList = new ArrayList<Machine>();
+    public ArrayList<String> machineArray = new ArrayList<String>();
+
+    ProgressDialog progressDialog;
+    JSONArray serverCSVrecords = null;
+
+    public String[] latestRecords;
+    public String[] allCSVRecords;
+
+    private int selectedTab = 0;
+
+    // for swipe
+    public SwipeRefreshLayout mSwipeRefreshLayout = null;
+
+    // for date time
+    TextView updateDateTime;
+
     //For the sorting tab
     private TabLayout tabLayout;
-    ViewPager viewPager;
-    ViewTabPageAdapter viewTabPagerAdapter;
 
     public ListFragment() {
     }
@@ -83,35 +103,234 @@ public class ListFragment extends Fragment {
         context = getContext();
         View rootView = inflater.inflate(R.layout.fragment_list, container, false);
 
-        // Get the ViewPager and set it's PagerAdapter so that it can display items
-        viewPager = (ViewPager) rootView.findViewById(R.id.viewpager);
-        viewTabPagerAdapter = new ViewTabPageAdapter(getActivity().getSupportFragmentManager(),
-                this.getContext());
-        viewPager.setAdapter(viewTabPagerAdapter);
-        viewPager.setOffscreenPageLimit(2);
-
         // Give the TabLayout the ViewPage
         tabLayout = (TabLayout) rootView.findViewById(R.id.list_tabs);
-        tabLayout.setupWithViewPager(viewPager);
 
+        // Retrieve the SwipeRefreshLayout and ListView instances
+        mSwipeRefreshLayout = (SwipeRefreshLayout) rootView.findViewById(R.id.swipeRefresh);
+        updateDateTime= (TextView) rootView.findViewById(R.id.textViewUpdateDateTime);
+
+
+        //retrieving data using bundle
+        Bundle bundle = getArguments();
+        if (bundle != null) {
+            //Log.i(TAG + " Machine Name ", String.valueOf(bundle.getStringArrayList("name")));
+            machineArray.add(String.valueOf(bundle.getStringArrayList("name")));
+        }
+
+
+        // set up list with listadapter
+        listViewListing = (ListView) rootView.findViewById(R.id.ListView);
+        adapter = new CustomAdapter(getActivity(), R.layout.fragment_list, myMachineList);
+        listViewListing.setAdapter(adapter);
+
+        //////////////////swipe///////////////
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Log.i("REFRESH", "onRefresh called from SwipeRefreshLayout");
+                myMachineList.clear();
+                getCSVData();
+            }
+        });
+        ////// when click on the item   //////////////////////
+        listViewListing.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ViewGroup viewgrp = (ViewGroup) view;
+                TextView intentMachineID = (TextView) viewgrp.findViewById(R.id.textViewmachineid);
+                TextView intentTemp = (TextView) viewgrp.findViewById(R.id.textViewTemp);
+                TextView intentVelo = (TextView) viewgrp.findViewById(R.id.textViewVelocity);
+
+                DetailsFragment details = new DetailsFragment();
+                //using Bundle to send data
+                Bundle bundle2 = new Bundle();
+                bundle2.putString("name", intentMachineID.getText().toString());
+                bundle2.putString("temp", intentTemp.getText().toString());
+                bundle2.putString("velo", intentVelo.getText().toString());
+                details.setArguments(bundle2); //data being send to MachineListFragment
+                //Edited by kerui
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.relativelayoutfor_fragment, details);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Log.e("TAG", "on tab selected");
+                selectedTab = tabLayout.getSelectedTabPosition();
+                updateList();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        progressDialog = new ProgressDialog(getActivity());
+        getCSVData();
 
         return rootView;
     }
 
+    //Added by Kerui
+    public void getCSVData(){
+        class GetCSVDataJSON extends AsyncTask<Void, Void, JSONObject> {
 
-    @Override
-    public void onPause() {
-        Log.e(TAG, "onPause");
-        viewPager.invalidate();
-        viewTabPagerAdapter.notifyDataSetChanged();
-        viewPager.setAdapter(null);
-        super.onPause();
+            URL encodedUrl;
+            HttpURLConnection urlConnection = null;
+
+            String url = "http://itpsenmon.net23.net/readFromCSV.php";
+
+            JSONObject responseObj;
+
+            @Override
+            protected void onPreExecute() {
+                progressDialog.setMessage("Loading Records...");
+                progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                progressDialog.setIndeterminate(false);
+                progressDialog.show();
+            }
+
+            @Override
+            protected JSONObject doInBackground(Void... params) {
+                try {
+                    encodedUrl = new URL(url);
+                    urlConnection = (HttpURLConnection) encodedUrl.openConnection();
+                    urlConnection.setDoInput(true);
+                    urlConnection.setDoOutput(true);
+                    urlConnection.setUseCaches(false);
+                    urlConnection.setRequestProperty("Content-Type", "application/json");
+                    urlConnection.connect();
+
+                    InputStream input = urlConnection.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line);
+                    }
+                    Log.d("doInBackground(Resp)", result.toString());
+                    responseObj = new JSONObject(result.toString());
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }finally {
+                    urlConnection.disconnect();
+                }
+
+                return responseObj;
+            }
+
+            @Override
+            protected void onPostExecute(JSONObject result){
+                super.onPostExecute(result);
+                getCSVRecords(result);
+                updateList();                   // display list with sorted values
+                progressDialog.dismiss();
+
+                // for swipe refresh to dismiss the loading icon
+                mSwipeRefreshLayout.setRefreshing(false);
+                // display the date time
+                dateTime();
+            }
+        }
+        GetCSVDataJSON g = new GetCSVDataJSON();
+        g.execute();
     }
 
-    @Override
-    public void onResume() {
-        Log.e(TAG, "onResume");
-        viewPager.setAdapter(viewTabPagerAdapter);
-        super.onResume();
+    //Get the server CSV records
+    public void getCSVRecords(JSONObject jsonObj)
+    {
+        try {
+            serverCSVrecords = jsonObj.getJSONArray(TAG_RESULTS);
+
+            String cleanupLatestRecords;
+
+            //remove all unwanted symbols and text
+            cleanupLatestRecords = serverCSVrecords.toString().replaceAll(",false]]", "").replace("[[", "").replace("[", "").replace("]]", "").replace("\"","").replace("]","");
+            //split different csv records, the ending of each csv record list is machineID.csv
+            allCSVRecords = cleanupLatestRecords.split(".csv,");
+            //loop through each csv and get the latest records and split each field
+            for(String record : allCSVRecords)
+            {
+                latestRecords = record.split(",");
+
+                Machine machine = new Machine(latestRecords[9].replace(".csv",""),latestRecords[0],latestRecords[1],latestRecords[2],latestRecords[3],latestRecords[4],
+                        latestRecords[5],latestRecords[6],latestRecords[7],latestRecords[8],"22");
+
+                myMachineList.add(machine);
+
+            }
+
+            Log.d("cleanupLatestRecords: ", cleanupLatestRecords);
+            Log.d("CSVRecords2: ", allCSVRecords[1]);
+            Log.d("LatestRecords: ", latestRecords[0]);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        dateTime();
+    }
+
+    public void dateTime()
+    {
+        String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+        Log.i("DATETIME", ""+currentDateTimeString.toString());
+
+
+        updateDateTime.setText("Updated on:" +currentDateTimeString);
+    }
+
+    public void updateList() {
+        switch(selectedTab)
+        {
+            case 0:
+                Log.i("SWITCH", "0 , Sort machine id");
+                Collections.sort(myMachineList, new Comparator<Machine>() {
+                    public int compare(Machine m1, Machine m2) {
+                        return m1.getMachineID().compareTo(m2.getMachineID());
+                    }
+                });
+                break;
+
+            case 1:
+                Log.i("SWITCH", "1 , Sort temp");
+                Collections.sort(myMachineList, new Comparator<Machine>() {
+                    public int compare(Machine m1, Machine m2) {
+                        return Double.compare(Double.parseDouble(m2.getmachineTemp()), Double.parseDouble(m1.getmachineTemp()));
+                    }
+                });
+                Log.i("Sort", "Sorting temp done");
+                break;
+
+            case 2:
+                Log.i("SWITCH", "2 , Sort velo");
+                Collections.sort(myMachineList, new Comparator<Machine>() {
+                    public int compare(Machine m1, Machine m2) {
+                        return Double.compare(Double.parseDouble(m2.getmachineVelo()), Double.parseDouble(m1.getmachineVelo()));
+                    }
+                });
+                break;
+
+            default:
+                break;
+        }
+        adapter.notifyDataSetChanged();
     }
 }
